740dacf730cff832420e44cd5734c846ee58e949
josiah <918780@dpsk12.net>
2022-03-31 14:43:59 -0600

    created basic server

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const express = require('express')
+
+const app = new express
+
+const port = 3000
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/public/index.html')
+})
+
+app.get('/script.js',(req,res)=>{
+    res.sendFile(__dirname+'/public/script.js')
+})
+
+app.get('/style.css',(req,res)=>{
+    res.sendFile(__dirname+'/public/style.css')
+})
+
+app.listen(port, ()=>{
+    console.log(`listening on port ${port}`)
+})
